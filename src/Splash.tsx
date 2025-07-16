2ad1a4192a6d96ce7651c055ef51ee11ca3d7082
tai <15724094+tai-314@users.noreply.github.com>
2025-07-15 21:57:28 -0400

    hide logo on splash

--- src/Splash.tsx
+++ src/Splash.tsx
@@ -15,7 +15,10 @@ import Animated, {
   withTiming,
 } from 'react-native-reanimated'
 import {useSafeAreaInsets} from 'react-native-safe-area-context'
-import Svg, {Path, SvgProps} from 'react-native-svg'
+import Svg, {
+  // Path,
+  type SvgProps,
+} from 'react-native-svg'
 import {Image} from 'expo-image'
 import * as SplashScreen from 'expo-splash-screen'
 
@@ -39,10 +42,10 @@ export const Logo = React.forwardRef(function LogoImpl(props: SvgProps, ref) {
       ref={ref}
       viewBox="0 0 64 66"
       style={[{width, height}, props.style]}>
-      <Path
+      {/* <Path
         fill={props.fill || '#fff'}
         d="M13.873 3.77C21.21 9.243 29.103 20.342 32 26.3v15.732c0-.335-.13.043-.41.858-1.512 4.414-7.418 21.642-20.923 7.87-7.111-7.252-3.819-14.503 9.125-16.692-7.405 1.252-15.73-.817-18.014-8.93C1.12 22.804 0 8.431 0 6.488 0-3.237 8.579-.18 13.873 3.77ZM50.127 3.77C42.79 9.243 34.897 20.342 32 26.3v15.732c0-.335.13.043.41.858 1.512 4.414 7.418 21.642 20.923 7.87 7.111-7.252 3.819-14.503-9.125-16.692 7.405 1.252 15.73-.817 18.014-8.93C62.88 22.804 64 8.431 64 6.488 64-3.237 55.422-.18 50.127 3.77Z"
-      />
+      /> */}
     </Svg>
   )
 })
